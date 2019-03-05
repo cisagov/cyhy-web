df49d0b01545ad018852010a1d1af474667b2b27
Felddy <markf+github@geekpad.com>
2019-03-04 22:23:24 -0500

    Basic login function working to backend API server!

--- src/store.js
+++ src/store.js
@@ -1,16 +1,73 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import gql from 'graphql-tag';
+
+import graphqlClient from '@/utils/graphql';
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-
+export const mutations = {
+  setTokens(state, {accessToken, refreshToken, viewer}) {
+    state.accessToken = accessToken;
+    state.refreshToken = refreshToken;
+    state.viewer = viewer;
   },
-  mutations: {
-
-  },
-  actions: {
-
+  setViewer(state, username) {
+    state.viewer = username;
   }
-})
+};
+
+export const actions = {
+  async auth({
+    commit
+  }, {username, password}) {
+    const response = await graphqlClient.mutate({
+      // It is important to not use the
+      // ES6 template syntax for variables
+      // directly inside the `gql` query,
+      // because this would make it impossible
+      // for Babel to optimize the code.
+      mutation: gql `
+        mutation ($username: String!, $password: String!){
+          auth(username: $username, password: $password) {
+            result {
+              __typename
+              ... on AuthField {
+                accessToken
+                refreshToken
+                username
+                message
+              }
+              ... on ResponseMessageField {
+                message
+                isSuccess
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        username: username,
+        password: password
+      }
+    })
+    // Trigger the `setTokens` mutation
+    // which is defined above.
+    // TODO handle failure
+    commit('setTokens', {accessToken: response.data.auth.result.accessToken,
+      refreshToken: response.data.auth.result.refreshToken,
+      viewer: response.data.auth.result.username});
+  }
+};
+
+export const state = {
+  viewer: null,
+  accessToken: null,
+  refreshToken: null
+};
+
+export default new Vuex.Store({
+  mutations,
+  actions,
+  state,
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="login">
     <h1>This is the login page</h1>
+    <p>accessToken: <code>{{accessToken}}</code></p>
+    <p>refreshToken: <code>{{refreshToken}}</code></p>
+    <p>username: <code>{{viewer}}</code></p>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Login',
+  computed: {
+    ...mapState(['accessToken', 'refreshToken', 'viewer']),
+  },
+  beforeCreate() {
+    // TODO get from a form
+    this.$store.dispatch('auth', {username: 'felddy', password:'foobar'});
+  },
+};
+</script>
