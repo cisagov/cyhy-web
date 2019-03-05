ed561fea66a49267e2c2675d2dcd9f9a09b9dd3d
Felddy <markf+github@geekpad.com>
2019-03-05 16:04:55 -0500

    Apply route-level code splitting.

--- src/router.js
+++ src/router.js
@@ -1,33 +1,40 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+//import Home from "./views/Home.vue";
 
 Vue.use(Router);
+
+// route level code-splitting
+// this generates a separate chunk (component.[hash].js) for each route
+// which is lazy-loaded when the route is visited.
 
 export default new Router({
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue")
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
       path: "/login",
       name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (login.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "login" */ "./views/Login.vue")
+    },
+    {
+      path: "/secure",
+      name: "secure",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
