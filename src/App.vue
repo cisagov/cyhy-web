25e55d7ff0b6c5cedfc73a87de3300daa331cf46
Felddy <markf+github@geekpad.com>
2019-03-04 23:13:20 -0500

    Prettier installed with pre-commit

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/login">Login</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
