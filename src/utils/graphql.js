698ed385cd2c09009d87eaa22035df772d856f45
Felddy <markf+github@geekpad.com>
2019-03-12 15:02:25 -0400

    Move token refresh check out of Vuex getters. Getters are only recalculated when the state they are derived from changes.  Time is not one of these states.

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -3,8 +3,21 @@ import { from } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
 import { onError } from "apollo-link-error";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import jwtDecode from "jwt-decode";
 import store from "../store/";
+
+function shouldRefreshAccessToken() {
+  // determine if the access token should be refreshed
+  if (store.state.auth.accessToken == null) return false; // no token to refresh (guest)
+  const jwt = jwtDecode(store.state.auth.accessToken);
+  const local_time = Date.now() / 1000;
+  // calculate what the server time is using the previously measured skew
+  const server_time = local_time + store.state.auth.clockSkew;
+  // check if the token is within a valid time window
+  return server_time < jwt.nbf || server_time > jwt.exp;
+}
 
 // the link to the backend graphql API server
 const httpLink = new HttpLink({ uri: "http://localhost:5000/graphql" });
@@ -12,14 +25,29 @@ const httpLink = new HttpLink({ uri: "http://localhost:5000/graphql" });
 // a link to inject the accessToken into the authentication headers when possible
 const authLink = setContext((_, { headers }) => {
   const token = store.state.auth.accessToken;
+  console.log(">>> should refresh >>> ", shouldRefreshAccessToken());
   // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ""
-    }
-  };
+  if (token) {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${token}`
+      }
+    };
+  } else {
+    // the accessToken is not available, no authentication header
+    return;
+  }
 });
+
+// const refreshLink = new TokenRefreshLink({
+//   accessTokenField: 'accessToken',
+//   isTokenValidOrUndefined: () => { return !shouldRefreshAccessToken();},
+//   fetchAccessToken: () => Promise<Response>,
+//   handleFetch: (accessToken: string) => void,
+//   handleResponse?: (operation, accessTokenField) => response => any,
+//   handleError?: (err: Error) => void,
+// });
 
 // this link will handle errors and log the user out if required
 const logoutLink = onError(({ graphQLErrors, networkError }) => {
@@ -31,7 +59,7 @@ const logoutLink = onError(({ graphQLErrors, networkError }) => {
       );
       // our token probably expired, time to logout
       // TODO try to refresh first
-      store.dispatch("auth/logout");
+      // store.dispatch("auth/refresh");
     });
   if (networkError) {
     // eslint-disable-next-line no-console
