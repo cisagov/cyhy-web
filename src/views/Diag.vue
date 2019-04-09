665d3d6b1b6e5350a808760b8719eeb4f02dca33
Felddy <markf+github@geekpad.com>
2019-04-08 21:05:25 -0400

    Remove shame from diagnostics.

--- src/views/Diag.vue
+++ src/views/Diag.vue
@@ -1,27 +1,38 @@
 <template>
   <div class="diagnostics">
-    <h1>App Diagnostics</h1>
-    <p>
-      accessToken:
-      <a
-        :href="'https://jwt.io/#debugger-io?token=' + accessToken"
-        target="_blank"
-      >
-        <code>{{ accessToken }}</code></a
-      >
-    </p>
-    <p>
-      refreshToken:
-      <a
-        :href="'https://jwt.io/#debugger-io?token=' + refreshToken"
-        target="_blank"
-      >
-        <code>{{ refreshToken }}</code></a
-      >
-    </p>
-    <p>
-      username: <code>{{ viewer }}</code>
-    </p>
+    <h1 class="ui header">App Diagnostics</h1>
+    <div class="ui raised very padded text container segment">
+      <div class="ui top left attached label">
+        Refresh Token<a
+          class="detail"
+          :href="'https://jwt.io/#debugger-io?token=' + refreshToken"
+          target="_blank"
+          ><i class="external alternate icon"></i
+        ></a>
+      </div>
+      <p>
+        <code>{{ refreshToken }}</code>
+      </p>
+    </div>
+    <div class="ui raised very padded text container segment">
+      <div class="ui top left attached label">
+        Access Token<a
+          class="detail"
+          :href="'https://jwt.io/#debugger-io?token=' + accessToken"
+          target="_blank"
+          ><i class="external alternate icon"></i
+        ></a>
+      </div>
+      <p>
+        <code>{{ accessToken }}</code>
+      </p>
+    </div>
+    <div class="ui raised very padded text container segment">
+      <div class="ui top left attached label">User ID</div>
+      <p>
+        <code>{{ viewer }}</code>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -54,10 +65,10 @@ export default {
 
 <style scoped>
 code {
-  display: inline-block;
+  /* display: inline-block;
   padding: 10px;
   border: solid;
-  width: 50%;
+  width: 50%; */
   overflow-wrap: break-word;
 }
 </style>
